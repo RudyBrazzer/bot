612d2d95b2fa1c429bfd84a404f7e00c232d2060
perfect1on-boi <88454096+perfect1on-boi@users.noreply.github.com>
2022-03-06 14:52:28 +0530

    anitscam

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,9 +1,20 @@
 const GuildSchema = require('../models/guild-schema')
-
+const { MessageEmbed } = require("discord.js");
+const Discord = require('discord.js')
 const client = require('../index').client
 
 
 client.on('messageCreate', async message => {
+    const array = require(`../scam.json`)
+    if (array.some(word => message.content.toLowerCase().includes(word))) {
+        message.delete()
+        const embed = new MessageEmbed()
+            .setTitle("Scam detected")
+            .setColor("#ff0000")
+            .setDescription("Please don't send any scam messages. Thank you.")
+            .setFooter("Antiscam Automod")
+        message.channel.send({ embeds: [embed] }).then(m => { setTimeout(() => { m.delete() }, 60000) })
+    }
     if(message.author.bot || message.channel.type === 'DM') return
 
 
@@ -22,4 +33,5 @@ client.on('messageCreate', async message => {
         message.delete()
          message.channel.send(`Please do not use that word!`)
     }
+   
 })
